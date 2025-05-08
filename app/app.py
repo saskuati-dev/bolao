8166352f5afc98428cef51277bfe3c55ccf9d56e
saskuati-dev <100883337+saskuati-dev@users.noreply.github.com>
2025-05-07 22:56:03 -0300

    update logout and add create pool

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template, request, redirect, url_for, flash, session
+from flask import Flask,render_template, request, redirect, url_for, flash, session, abort
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 load_dotenv()
@@ -13,14 +14,56 @@ mongo_uri = os.environ.get("MONGO_URI")
 client = MongoClient(mongo_uri)
 db = client.get_default_database()
 users_db = db['users']
-
+boloes_db = db['boloes']
 
 @app.route('/')
 def home():
     if 'user_id' not in session:
         return redirect(url_for('login'))
+    
+    meus_boloes = boloes_db.find({'criador_id': session.get('user_id')})
     return render_template('index.html')
 
+
+@app.route('/gerenciar/<bolao_id>')
+def gerenciar(bolao_id):
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('home'))
+    
+    bolao = boloes_db.find_one({
+        '_id': bolao_id
+    })
+
+    if not bolao or bolao['criador_id']!= user_id:
+        abort(403)
+    
+    return render_template('gerenciar_bolao.html', bolao=bolao)
+
+@app.route('criar_bolao', methods=["GET", "POST"])
+def criar_bolao():
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        descricao = request.form.get('descricao','')
+        user_name = session.get('user_name', '')  
+
+        novo_bolao = {  
+            'nome': nome,  
+            'descricao': descricao,  
+            'criador_id': user_id,  
+            'criador_nome': user_name,  
+            'participantes': [],  
+            'rodadas': [],  
+            'criado_em': datetime.utcnow().isoformat()  
+        }
+        resultado = boloes_db.insert_one(novo_bolao)  
+        flash('Bolão criado com sucesso!', 'success')  
+        return redirect(url_for('detalhes_bolao', bolao_id=str(resultado.inserted_id)))  
+    return render_template('criar_bolao.html')
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -57,6 +100,7 @@ def login():
         if user and check_password_hash(user['password'], senha):
             flash('Login successful.', 'success')
             session['user_id'] = str(user['_id'])
+            session['user_name'] = str(user['name'])
             return redirect(url_for('home'))
         else:
             flash('Invalid username or password. Please try again.', 'danger')
@@ -67,6 +111,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
+    session.pop('user_name', None)
     flash('Você saiu da sua conta.')  
     return redirect(url_for('login'))
 
